4915980dadc7732d373b75039e2f3e0f735c9f71
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-06 21:46:32 -0300

    Feat: Created and modification some methodes

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('usuarios/', include('usuarios.urls')),
+    path('empresarios/', include('empresarios.urls')),
 ]
